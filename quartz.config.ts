135fb1b4e5dab175b6b466a53346dda8798116d3
9g9glalala <kyo0486@gmail.com>
2025-07-20 13:15:24 +0900

    Quartz sync: Jul 20, 2025, 1:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: 'goatcounter', websiteId: '9g9glalala',
+      provider: 'google', tagId: 'G-EFFWH0Y07G',
     },
     locale: "ja-JP",
     baseUrl: "quartz.jzhao.xyz",
